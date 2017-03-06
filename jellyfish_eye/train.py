32829916de77feabea455e6924f92f63bad3a584
ROjima1 <rojima1@gmail.com>
2017-03-06 12:51:10 +0900

    some bugs are fixed.

--- jellyfish_eye/train.py
+++ jellyfish_eye/train.py
@@ -3,6 +3,8 @@ import jellyfish_eye.model as model
 import tensorflow as tf
 
 
+tf.logging.set_verbosity(tf.logging.INFO)
+
 train_data_set, validation_data_set, _ = data_sets.load()
 
 inputs = model.inputs
@@ -16,7 +18,7 @@ accuracy = model.accuracy(logits)
 global_step = tf.contrib.framework.get_or_create_global_step()
 inc_global_step = tf.assign(global_step, tf.add(global_step, 1))
 summary = tf.summary.merge_all()
-supervisor = tf.train.Supervisor(logdir='logs', save_model_secs=60, save_summaries_secs=60, summary_op=None)
+supervisor = tf.train.Supervisor(logdir='logs', summary_op=None)
 
 with supervisor.managed_session() as session:
     while not supervisor.should_stop():
@@ -24,13 +26,18 @@ with supervisor.managed_session() as session:
 
         train_inputs, train_labels = train_data_set.next_batch(20)
         session.run(train, feed_dict={inputs: train_inputs, labels: train_labels, is_training: True})
-        
+
         if global_step_value % 10 == 0:
             supervisor.summary_computed(session, session.run(summary, feed_dict={inputs: train_inputs, labels: train_labels, is_training: True}))
-            
+
             print('global step {0:>4d}: train accuracy = {1:.4f}, validation accuracy = {2:.4f}.'.format(
                 global_step_value,
                 session.run(accuracy, feed_dict={inputs: train_inputs, labels: train_labels}),
                 session.run(accuracy, feed_dict={inputs: validation_data_set.inputs, labels: validation_data_set.labels})))
 
         session.run(inc_global_step)
+
+
+# もしWindows環境でlib\site-packages\tensorflow\python\training\supervisor.pyの1044行が例外を吐く場合は、1044行目を以下のように修正してみてください。
+#     steps_per_sec = added_steps / elapsed_time if elapsed_time != 0 else 0
+# どうやら、time.time()の仕様が、Windows環境とLinux/Mac環境で違うみたい……。
